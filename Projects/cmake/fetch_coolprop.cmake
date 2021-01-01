9f1e38034eba992518b1f3e379f24ecaba3eab2f
Jorrit Wronski <jowr@ipu.dk>
2021-01-01 22:59:22 +0100

    Use the source files directly

--- Projects/cmake/fetch_coolprop.cmake
+++ Projects/cmake/fetch_coolprop.cmake
@@ -1,7 +1,5 @@
 include(FetchContent)
 
-
-
 # Define the configuration variables
 if(CMAKE_SIZEOF_VOID_P MATCHES "8")
   set(COOLPROP_BITNESS "64")
@@ -30,37 +28,15 @@ FetchContent_Declare(
 # will be defined and available to the rest of the build
 #FetchContent_MakeAvailable(coolprop)
 
-# FetchContent_GetProperties(coolprop)
-# if(NOT coolprop_POPULATED)
-  # FetchContent_Populate(coolprop)
-  # add_subdirectory(${coolprop_SOURCE_DIR} ${coolprop_BINARY_DIR})
-# endif()
-
-# FetchContent_GetProperties(coolprop)
+FetchContent_GetProperties(coolprop)
 if(NOT coolprop_POPULATED)
   FetchContent_Populate(coolprop)
-  if(ADD_COOLPROP_OBJECT)
-    message(STATUS "Preparing CoolProp object files in \"${coolprop_BINARY_DIR}\"")
-    execute_process(
-      COMMAND ${CMAKE_COMMAND} --build ${coolprop_BINARY_DIR} --target CoolProp --config Release
-      RESULT_VARIABLE rv
-    )
-    message("rv='${rv}'")
-    execute_process(
-      COMMAND ${CMAKE_COMMAND} --build ${coolprop_BINARY_DIR} --target CoolProp --config Debug
-      RESULT_VARIABLE rv
-    )
-    message("rv='${rv}'")
-  endif()
   add_subdirectory(${coolprop_SOURCE_DIR} ${coolprop_BINARY_DIR})
 endif()
 
 if(ADD_COOLPROP_OBJECT)  
-  list(APPEND COOLPROP_INCLUDES "${coolprop_SOURCE_DIR}")
-  list(APPEND COOLPROP_INCLUDES "${coolprop_SOURCE_DIR}/include")
-  list(APPEND COOLPROP_INCLUDES "${coolprop_SOURCE_DIR}/externals/msgpack-c/include")
-  list(APPEND COOLPROP_INCLUDES "${coolprop_SOURCE_DIR}/externals/fmtlib")
-  set(COOLPROP_LINKER_INPUT $<TARGET_OBJECTS:CoolProp>)
+  get_target_property(COOLPROP_INCLUDES CoolProp INCLUDE_DIRECTORIES)
+  get_target_property(COOLPROP_LINKER_INPUT CoolProp SOURCES)
   set(COOLPROP_LIBRARY_FILE "")
 elseif(ADD_COOLPROP_STATIC)
   list(APPEND COOLPROP_INCLUDES "${coolprop_SOURCE_DIR}")
